392567b0d625fb17796afdeb1a0e555559564a6c
Milan <jevga.milan@gmail.com>
2025-04-10 14:40:04 +0200

    Initial commit.

--- src/main/java/com/example/todo/config/OpenApiConfig.java
+++ src/main/java/com/example/todo/config/OpenApiConfig.java
@@ -1,7 +1,8 @@
 package com.example.todo.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
@@ -9,6 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "DemoApp API", version = "v1"))
+
+public class OpenApiConfig {
+}
+
+/*@Configuration
 public class OpenApiConfig {
 
     @Bean
@@ -30,4 +37,4 @@ public class OpenApiConfig {
                         )
                 );
     }
-}
+}*/
